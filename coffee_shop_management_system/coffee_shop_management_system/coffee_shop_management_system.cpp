1925dd720a3c53a691f293a2442f56c496cf7cbd
PhucHoang200 <phucphuc2004444@gmail.com>
2025-11-09 18:22:18 +0700

    Update first project

---
+++ coffee_shop_management_system/coffee_shop_management_system/coffee_shop_management_system.cpp
@@ -0,0 +1,11 @@
+#include "coffee_shop_management_system.h"
+
+coffee_shop_management_system::coffee_shop_management_system(QWidget *parent)
+    : QMainWindow(parent)
+{
+    ui.setupUi(this);
+}
+
+coffee_shop_management_system::~coffee_shop_management_system()
+{}
+
